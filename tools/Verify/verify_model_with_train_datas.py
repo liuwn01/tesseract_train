322a2ab1275d993b2aab2b3f79d642e615a0a8f7
Wei Ning Liu <v-liuweining@microsoft.com>
2024-11-07 13:04:16 +0800

    Add a script to validate the model using the training datas

--- tools/Verify/verify_model_with_train_datas.py
+++ tools/Verify/verify_model_with_train_datas.py
@@ -8,6 +8,8 @@ import os
 import random
 import pytesseract
 from difflib import SequenceMatcher
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from threading import Lock
 
 TXT_TRAIN = "./list.train"
 TXT_EVAL = "./list.eval"
@@ -21,13 +23,6 @@ def check_string_in_text(file_path, target_string):
     with open(file_path, 'r', encoding='utf-8') as file:
         return any(target_string in line for line in file)
 
-def get_unique_characters(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read().replace('\n', '').replace('\r', '')
-
-    unique_chars = ''.join(sorted(set(content)))
-    return unique_chars
-
 def read_gt_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read().replace('\n', '').replace('\r', '')
@@ -39,51 +34,92 @@ def save_result(file_path, lines):
 def image_to_string(filepath, lang):
     return pytesseract.image_to_string(Image.open(filepath), lang=lang).replace(" ","").replace('\n', '').replace('\r', '')
 
+def get_result_file(file_path):
+    if os.path.exists(file_path):
+        return open(file_path, 'a', encoding='utf-8')
+    else:
+        return open(file_path, 'w', encoding='utf-8')
+
+def handle_tasks(task):
+    file_prefix = task["file_prefix"]
+    gt_index_prefix = task["gt_index_prefix"]
+    filepath = task["filepath"]
+
+    print(f"processing: {filepath}")
+    tesseract_parsed_string = image_to_string(filepath, TESSERACT_LANG)
+    gt_string = read_gt_file(f"{file_prefix}.gt.txt")
+    isInTrain = check_string_in_text(TXT_TRAIN, f"{gt_index_prefix}.lstmf")
+    isInEval = check_string_in_text(TXT_EVAL, f"{gt_index_prefix}.lstmf")
+    # print(gt_index_prefix,isInTrain,isInEval)
+    # print(gt_string, "|||", tesseract_parsed_string, SequenceMatcher(None, gt_string, tesseract_parsed_string).ratio())
+    isMatched = False
+
+    ratio = SequenceMatcher(None, gt_string, tesseract_parsed_string).ratio()
+    if ratio > 0.9999 or gt_string == tesseract_parsed_string:
+        try:
+            isMatched = True
+            shutil.move(f"{file_prefix}.tif", f"{TEMP_FOLDER}/matched")
+            shutil.move(f"{file_prefix}.box", f"{TEMP_FOLDER}/matched")
+            shutil.move(f"{file_prefix}.gt.txt", f"{TEMP_FOLDER}/matched")
+        except Exception as e:
+            print(f"Move file error: {file_prefix}")
+    else:
+        try:
+            shutil.move(f"{file_prefix}.tif", f"{TEMP_FOLDER}/unmatched")
+            shutil.move(f"{file_prefix}.box", f"{TEMP_FOLDER}/unmatched")
+            shutil.move(f"{file_prefix}.gt.txt", f"{TEMP_FOLDER}/unmatched")
+        except Exception as e:
+            print(f"Move file error: {file_prefix}")
+
+    with lock:
+        global total_items,error_items,result_file
+        total_items += 1
+        if not isMatched:
+            error_items += 1
+        result_file.write(f"{gt_index_prefix},{int(isMatched)},{ratio},{int(isInTrain)},{int(isInEval)},'{gt_string}','{tesseract_parsed_string}'\n")
+        result_file.flush()
+
+
 if os.path.exists(TEMP_FOLDER):
     shutil.rmtree(TEMP_FOLDER)
 os.makedirs(f"{TEMP_FOLDER}/matched", exist_ok=True)
 os.makedirs(f"{TEMP_FOLDER}/unmatched", exist_ok=True)
 
-unichars = get_unique_characters(TXT_GT)
-
 total_items = 0
 error_items = 0
+lock = Lock()
+max_concurrent_tasks = os.cpu_count()
+tasks=[]
+result_file = None
 
-summarize = []
-for fpathe,dirs,fs in os.walk(GROUPD_TRUTH_FOLDER):
-    for f_name in fs:
-        filepath=os.path.join(fpathe,f_name)
-        #print(f"{fpathe} ==> {f_name}")
-        if filepath.endswith('.tif'):
-            file_prefix = filepath.replace(".tif","")
-            gt_index_prefix = f_name.replace(".tif","")
-            tesseract_parsed_string = image_to_string(filepath, TESSERACT_LANG)
-            gt_string = read_gt_file(f"{file_prefix}.gt.txt")
-            isInTrain = check_string_in_text(TXT_TRAIN, f"{gt_index_prefix}.lstmf")
-            isInEval = check_string_in_text(TXT_EVAL, f"{gt_index_prefix}.lstmf")
-            #print(gt_index_prefix,isInTrain,isInEval)
-            #print(gt_string, "|||", tesseract_parsed_string, SequenceMatcher(None, gt_string, tesseract_parsed_string).ratio())
-            isMatched = False
-            total_items += 1
-            ratio = SequenceMatcher(None, gt_string, tesseract_parsed_string).ratio()
-            if ratio > 0.9999 or gt_string == tesseract_parsed_string:
-                try:
-                    shutil.move(f"{file_prefix}.tif", f"{TEMP_FOLDER}/matched")
-                    shutil.move(f"{file_prefix}.box", f"{TEMP_FOLDER}/matched")
-                    shutil.move(f"{file_prefix}.gt.txt", f"{TEMP_FOLDER}/matched")
-                except Exception as e:
-                    print(f"Move file error: {file_prefix}")
-            else:
-                try:
-                    error_items += 1
-                    shutil.move(f"{file_prefix}.tif", f"{TEMP_FOLDER}/unmatched")
-                    shutil.move(f"{file_prefix}.box", f"{TEMP_FOLDER}/unmatched")
-                    shutil.move(f"{file_prefix}.gt.txt", f"{TEMP_FOLDER}/unmatched")
-                except Exception as e:
-                    print(f"Move file error: {file_prefix}")
+try:
+    result_file = get_result_file("./result.csv")
 
-            summarize.append(f"{gt_index_prefix},{isMatched},{ratio},{int(isInTrain)},{int(isInEval)},{tesseract_parsed_string},{gt_string}\n")#{os.linesep}
+    for fpathe,dirs,fs in os.walk(GROUPD_TRUTH_FOLDER):
+        for f_name in fs:
+            filepath=os.path.join(fpathe,f_name)
+            #print(f"{fpathe} ==> {f_name}")
+            if filepath.endswith('.tif'):
+                file_prefix = filepath.replace(".tif","")
+                gt_index_prefix = f_name.replace(".tif","")
+                tasks.append(
+                    {
+                        "filepath": filepath,
+                        "file_prefix": file_prefix,
+                        "gt_index_prefix": gt_index_prefix
+                    }
+                )
 
-print(f"Total: {total_items}; Error_Items: {error_items}; error_ratio: {error_items/total_items*100.0}%")
+    with ThreadPoolExecutor(max_workers=max_concurrent_tasks) as executor:
+        futures = [executor.submit(handle_tasks, item) for item in tasks]
+        for future in as_completed(futures):
+            pass
 
-save_result("./result.csv", summarize)
+    #print(f"Total: {total_items}; Error_Items: {error_items}")
+    print("Done.")
+except Exception as e:
+    print(f"Failed to verify! {e}")
+finally:
+    result_file.close()
+
+
